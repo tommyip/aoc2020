4d22a3652ac6e857e6a6beacee384ea401d1ab73
Thomas Ip <thomas@ipthomas.com>
2020-12-01 14:28:44 +0800

    Simply use list instead of vector for day 1

--- bin/day1_part1.ml
+++ bin/day1_part1.ml
@@ -1,17 +1,15 @@
-let find_2020_sum_pair nums =
-  CCVector.find_map
+let product_of_2020_pair nums =
+  CCList.find_map
     (fun num1 ->
-      CCVector.find_map
+      CCList.find_map
         (* Assume it is fine if the two numbers are the same entry *)
-        (fun num2 -> if num1 + num2 = 2020 then Some (num1, num2) else None)
+        (fun num2 -> if num1 + num2 = 2020 then Some (num1 * num2) else None)
         nums)
     nums
 
 let () =
   open_in "inputs/day1.txt"
-  |> CCIO.read_lines_gen
-  |> Gen.map int_of_string
-  |> CCVector.of_gen
-  |> find_2020_sum_pair
-  |> CCOpt.map (fun (x, y) -> x * y)
+  |> CCIO.read_lines_l
+  |> List.map int_of_string
+  |> product_of_2020_pair
   |> CCOpt.pp CCFormat.int Format.std_formatter
--- bin/day1_part2.ml
+++ bin/day1_part2.ml
@@ -1,20 +1,18 @@
-let find_2020_sum_triple nums =
-  CCVector.find_map
+let product_of_2020_triple nums =
+  CCList.find_map
     (fun num1 ->
-      CCVector.find_map
+      CCList.find_map
         (fun num2 ->
-          CCVector.find_map
+          CCList.find_map
             (fun num3 ->
-              if num1 + num2 + num3 = 2020 then Some (num1, num2, num3) else None)
+              if num1 + num2 + num3 = 2020 then Some (num1 * num2 * num3) else None)
             nums)
         nums)
     nums
 
 let () =
   open_in "inputs/day1.txt"
-  |> CCIO.read_lines_gen
-  |> Gen.map int_of_string
-  |> CCVector.of_gen
-  |> find_2020_sum_triple
-  |> CCOpt.map (fun (x, y, z) -> x * y * z)
+  |> CCIO.read_lines_l
+  |> List.map int_of_string
+  |> product_of_2020_triple
   |> CCOpt.pp CCFormat.int Format.std_formatter
