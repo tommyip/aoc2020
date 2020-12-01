74395e164107e1aecf565cdf3e6d5c4fd285bf06
Thomas Ip <thomas@ipthomas.com>
2020-12-01 13:36:59 +0800

    Day 1 part 1

--- bin/day1_part1.ml
+++ bin/day1_part1.ml
@@ -1,3 +1,17 @@
+let find_2020_sum_pair nums =
+  CCVector.find_map
+    (fun num1 ->
+      CCVector.find_map
+        (* Assume it is fine if the two numbers are the same entry *)
+        (fun num2 -> if num1 + num2 = 2020 then Some (num1, num2) else None)
+        nums)
+    nums
+
 let () =
-  print_endline "Hello Santa!";
-  print_endline Lib.Helper.goodbye_msg
+  open_in "inputs/day1.txt"
+  |> CCIO.read_lines_gen
+  |> Gen.map int_of_string
+  |> CCVector.of_gen
+  |> find_2020_sum_pair
+  |> CCOpt.map (fun (x, y) -> x * y)
+  |> CCOpt.pp CCFormat.int Format.std_formatter
