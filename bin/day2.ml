b5d5071e2ec044c43a5b2fdeec0da27058cff647
Thomas Ip <thomas@ipthomas.com>
2020-12-02 15:27:44 +0800

    Day 2

---
+++ bin/day2.ml
@@ -0,0 +1,29 @@
+type entry =
+  { lo: int;
+    hi: int;
+    letter: char;
+    password: string
+  }
+
+let parse_policy_password input =
+  Scanf.sscanf input "%u-%u %c: %s"
+    (fun lo hi letter password -> { lo; hi; letter; password })
+
+let is_valid_part1 entry =
+  let n = CCString.to_list entry.password
+    |> List.find_all ((=) entry.letter)
+    |> List.length
+  in n >= entry.lo && n <= entry.hi
+
+let is_valid_part2 entry =
+  (entry.password.[entry.lo - 1] = entry.letter) <>
+  (entry.password.[entry.hi - 1] = entry.letter)
+
+let () =
+  let entries = open_in "inputs/day2.txt"
+    |> CCIO.read_lines_l
+    |> List.map parse_policy_password
+  in
+  let part1 = entries |> List.filter is_valid_part1 |> List.length in
+  let part2 = entries |> List.filter is_valid_part2 |> List.length in
+  Printf.printf "%d %d\n" part1 part2
