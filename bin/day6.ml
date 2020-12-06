66aa665c1fee775e07aa49445592fd754e02db9c
Thomas Ip <thomas@ipthomas.com>
2020-12-06 16:04:34 +0800

    Day 6 use set union & intersection

--- bin/day6.ml
+++ bin/day6.ml
@@ -1,19 +1,17 @@
-module CharMap = Map.Make(Char)
-
-let map_input =
-  CCString.fold
-    (fun map question ->
-      CharMap.update question (function Some x -> Some (x + 1) | None -> Some 1) map)
-    CharMap.empty
+module CharSet = CCSet.Make(Char)
 
 let () =
-  let map = open_in "inputs/day6.txt"
-    |> CCIO.read_all
-    |> CCString.split ~by:"\n\n"
-    |> List.map (fun input ->
-        let group = CCString.replace ~sub:"\n" ~by:"" input in
-        (map_input group, List.length (CCString.lines input)))
+  let part =
+    let list_of_sets = open_in "inputs/day6.txt"
+      |> CCIO.read_all
+      |> CCString.rtrim
+      |> CCString.split ~by:"\n\n"
+      |> List.map (fun input ->
+          String.split_on_char '\n' input
+          |> List.map (CCString.fold (CCFun.flip CharSet.add) CharSet.empty))
+    in fun f ->
+      List.map (fun ls -> List.fold_left f (List.hd ls) ls |> CharSet.cardinal) list_of_sets
   in
-  let part1 = List.map (fun (m, _) -> CharMap.cardinal m) map in
-  let part2 = List.map (fun (m, n) -> CharMap.fold (fun _ x acc -> if x = n then acc + 1 else acc) m 0) map in
-  Printf.printf "%d %d\n" (Lib.List.sum part1) (Lib.List.sum part2)
+  let part1 = Lib.List.sum (part CharSet.union) in
+  let part2 = Lib.List.sum (part CharSet.inter) in
+  Printf.printf "%d %d\n" part1 part2
