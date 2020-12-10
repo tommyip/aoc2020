45cb64ac299fa09919f351ea82985e5a8530ebc8
Thomas Ip <thomas@ipthomas.com>
2020-12-10 16:30:12 +0800

    Memorize with CCCache in day 10

--- bin/day10.ml
+++ bin/day10.ml
@@ -1,22 +1,19 @@
 open Lib
 open Containers
 
-let rec ways i adapters cache =
-  match Hashtbl.find_opt cache i with
-  | None ->
-      let out =
-        if i = Array.length adapters - 1 then 1
-        else
-          let x = Array.get adapters i in
-          let subways j =
-            match Array.get_safe adapters (i + j) with
-            | Some y -> if (y - x) <= 3 then ways (i + j) adapters cache else 0
-            | None -> 0
-          in
-          subways 1 + subways 2 + subways 3
+let ways adapters =
+  let length = Array.length adapters in
+  let aux aux' i =
+    if i = length - 1 then 1
+    else
+      let x = Array.get adapters i in
+      let subways j =
+        match Array.get_safe adapters (i + j) with
+        | Some y -> if (y - x) <= 3 then aux' (i + j) else 0
+        | None -> 0
       in
-      Hashtbl.add cache i out; out
-  | Some x -> x
+      subways 1 + subways 2 + subways 3
+  in (CCCache.with_cache_rec (CCCache.lru ~eq:(=) length) aux) 0
 
 let () =
   let adapters = IO.read_lines_l stdin
@@ -28,5 +25,5 @@ let () =
     |> List.map (fun lst -> let (a, b) = MYList.to_pair lst in b - a)
     |> fun lst -> (List.count (( = ) 1) lst) * (List.count (( = ) 3) lst)
   in
-  let part2 = ways 0 (Array.of_list adapters) (Hashtbl.create (List.length adapters)) in
+  let part2 = ways (Array.of_list adapters) in
   Printf.printf "%d %d\n" part1 part2
