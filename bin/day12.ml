485d5da51eb89bb8ca126c05705fcebd385deac7
Thomas Ip <thomas@ipthomas.com>
2020-12-12 20:01:36 +0800

    Use complex numbers for day 12
    Source: https://www.reddit.com/r/adventofcode/comments/kbj5me/2020_day_12_solutions/gfhqlnh/?utm_source=reddit&utm_medium=web2x&context=3

--- bin/day12.ml
+++ bin/day12.ml
@@ -1,46 +1,29 @@
 open Containers
+open Complex
 
-let rad d = d *. Float.pi /. 180.
-
-let rotate (x, y) dr =
-  let original_r = atan2 y x in
-  let new_r = original_r +. dr in
-  let length = sqrt ((x ** 2.) +. (y ** 2.)) in
-  (length *. cos new_r, length *. sin new_r)
+let scale z multipler = { re=(z.re *. multipler); im=(z.im *. multipler) }
 
 let () =
   let input = IO.read_lines_l stdin
     |> List.map (String.take_drop 1)
     |> List.map (Pair.map_snd float_of_string)
   in
-  let part1 = input
-  |> List.fold_left (fun (x, y, dir) (action, value) ->
-    match action with
-    | "N" -> (x, y +. value, dir)
-    | "S" -> (x, y -. value, dir)
-    | "E" -> (x +. value, y, dir)
-    | "W" -> (x -. value, y, dir)
-    | "L" -> (x, y, dir +. (rad value))
-    | "R" -> (x, y, dir -. (rad value))
-    | "F" -> ((cos dir) *. value +. x, (sin dir) *. value +. y, dir)
-    | _ -> failwith ""
-    ) (0., 0., 0.)
-    |> fun (x, y, _) -> Float.abs x +. Float.abs y
+  let part x =
+    let (init, select) =
+      if x = 1 then (one, Pair.map_fst)
+      else ({ re=10.; im=1. }, Pair.map_snd)
+    in input
+    |> List.fold_left (fun (loc, dir) (action, value) ->
+        match action with
+        | "N" -> (loc, dir) |> select (add (scale i value))
+        | "S" -> (loc, dir) |> select (add (scale (neg i) value))
+        | "E" -> (loc, dir) |> select (add (scale one value))
+        | "W" -> (loc, dir) |> select (add (scale (neg one) value))
+        | "L" -> (loc, mul dir (pow i (scale one (value /. 90.))))
+        | "R" -> (loc, mul dir (pow (neg i) (scale one (value /. 90.))))
+        | "F" -> (add loc (scale dir value), dir)
+        | _ -> failwith "Unknown command"
+      ) (zero, init)
+    |> fun ({ re; im }, _) -> Float.abs re +. Float.abs im
   in
-
-  let part2 = input
-  |> List.fold_left (fun (x, y, wx, wy) (action, value) ->
-    match action with
-    | "N" -> (x, y, wx, wy +. value)
-    | "S" -> (x, y, wx, wy -. value)
-    | "E" -> (x, y, wx +. value, wy)
-    | "W" -> (x, y, wx -. value, wy)
-    | "L" -> let (wx, wy) = rotate (wx, wy) (rad value) in (x, y, wx, wy)
-    | "R" -> let (wx, wy) = rotate (wx, wy) (rad (-. value)) in (x, y, wx, wy)
-    | "F" -> (x +. wx *. value, y +. wy *. value, wx, wy)
-    | _ -> failwith ""
-    ) (0., 0., 10., 1.)
-    |> fun (x, y, _, _) -> Float.abs x +. Float.abs y
-  in
-
-  Printf.printf "%.0f %.0f\n" part1 part2
+  Printf.printf "%.0f %.0f\n" (part 1) (part 2)
