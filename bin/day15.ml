cb6554f83a79fce3a3218f6d1e17e9d22bbc127b
Thomas Ip <thomas@ipthomas.com>
2020-12-15 19:13:17 +0800

    Day 15 - use array instead of hashtbl

--- bin/day15.ml
+++ bin/day15.ml
@@ -1,29 +1,27 @@
 open Containers
 
-let () =
-  let start = IO.read_all stdin
-    |> String.rtrim
-    |> String.split_on_char ','
-    |> List.map int_of_string
-  in
-  let (first, last) = List.take_drop (List.length start - 1) start
-    |> Pair.map_snd List.hd
-  in
+let start =
+  IO.read_all stdin |> String.rtrim |> String.split_on_char ','
+  |> List.map int_of_string
 
-  let nth_number n =
-    let tbl = Hashtbl.of_seq (Seq.zip (List.to_seq first) (Seq.(1 -- List.length first))) in
-    let rec turn ith recent =
-      if ith = n + 1 then recent else
-      let next = match Hashtbl.get tbl recent with
-      | Some last_nth -> ith - 1 - last_nth
-      | None -> 0
+let start_less_last, last =
+  List.take_drop (List.length start - 1) start |> Pair.map_snd List.hd
+
+let nth n =
+  let tbl = Array.make n 0 in
+  List.iteri (fun i num -> tbl.(num) <- i + 1) start_less_last;
+  let rec turn ith recent =
+    if ith = n + 1 then recent
+    else
+      let next = match tbl.(recent) with
+        | 0 -> 0
+        | last_ith -> ith - 1 - last_ith
       in
-      Hashtbl.add tbl recent (ith - 1);
+      tbl.(recent) <- ith - 1;
       turn (ith + 1) next
-    in
-    turn (List.length start + 1) last
-  in
+  in turn (List.length start + 1) last
 
-  let part1 = nth_number 2020 in
-  let part2 = nth_number 30000000 in
-  Printf.printf "%d %d\n" part1 part2
+let part1 = nth 2020
+let part2 = nth 30000000
+
+let () = Printf.printf "%d %d\n" part1 part2
